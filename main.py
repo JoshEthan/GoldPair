b9f030fda9723e5b376e6803c5f5f06966215589
JoshEthan <joshuaeris@gmail.com>
2021-09-07 20:40:30 -0600

    Testing

--- main.py
+++ main.py
@@ -2,3 +2,4 @@ from binance import Client, ThreadedWebsocketManager, ThreadedDepthCacheManager
 import config
 print(config.api_key)
 #client = Client(api_key, api_secret)
+#client = Client(api_key, api_secret)
